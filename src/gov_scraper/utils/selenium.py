30b92b6e33c04f06d4688cfe95935676bd327e16
Yochay Cohen-Or <yochai.cohenor@gmail.com>
2025-08-06 14:30:38 +0300

    Ignore chrome binary

--- src/gov_scraper/utils/selenium.py
+++ src/gov_scraper/utils/selenium.py
@@ -43,7 +43,7 @@ class SeleniumWebDriver:
             chrome_options.add_argument('--disable-dev-shm-usage')
             chrome_options.add_argument('--disable-gpu')
             chrome_options.add_argument('--window-size=1920,1080')
-            chrome_options.add_argument(f"--binary-location={CHROME_BINARY}")
+            # chrome_options.add_argument(f"--binary-location={CHROME_BINARY}")
             chrome_options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
             
             # Automatically download and setup ChromeDriver
